d5440713ff9a84a9859ad07b2d4142a1a548bf6c
Soham Saha <sohamsaha291@gmail.com>
2025-06-27 23:52:41 +0530

    page changes

--- EVENTS (2)/script.js
+++ EVENTS (2)/script.js
@@ -17,39 +17,4 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
-
-  // Specific animation for #innovatia-2025
-  const innovatiaSection = document.querySelector('#innovatia-2025');
-  const innovatiaImage = innovatiaSection.querySelector('.event-image');
-  const innovatiaDescription = innovatiaSection.querySelector('.event-description');
-
-  if (innovatiaSection && innovatiaImage && innovatiaDescription) {
-    
-    const runAnimation = () => {
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: innovatiaSection,
-                start: 'top 60%',
-                toggleActions: 'play none none none',
-            }
-        });
-
-        tl.from(innovatiaImage, {
-            opacity: 0,
-            duration: 1.5,
-            ease: 'power2.inOut'
-        })
-        .from(innovatiaDescription, 
-            { 
-                xPercent: 110, 
-                opacity: 0, 
-                duration: 2, 
-                ease: 'power3.out' 
-            }, 
-            "<0.2"
-        );
-    };
-
-    window.addEventListener('load', runAnimation);
-  }
 });
